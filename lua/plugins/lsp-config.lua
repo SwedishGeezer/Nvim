6cd0dfa5d0f78c0f2f75d88eba7a7d801e8cc55a
Swedish Geezer <bondlurken97@gmail.com>
2024-02-29 20:20:06 +0100

    Fixed LSP's

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -23,9 +23,6 @@ return {
       lspconfig.tsserver.setup({
         capabilities = capabilities
       })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
       lspconfig.lua_ls.setup({
         capabilities = capabilities
       })
